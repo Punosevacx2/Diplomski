b438ecbbf6d1ffc54039714157695540aeaf7876
Punosevacx2 <milosiveljko.punosevac@gmail.com>
2026-06-07 19:45:59 +0200

    Seminarski dodat

--- Podaci/src/app.ts
+++ Podaci/src/app.ts
@@ -1,29 +1,29 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import recipeRoutes from "./routes/recipeRoutes.ts";
-import milvusRoutes from "./routes/milvusRoutes.ts";
-////import collectionRoutes from "./routes/collectionRoutes.ts";
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import recipeRoutes from "./routes/recipeRoutes.ts";
+// import milvusRoutes from "./routes/milvusRoutes.ts";
+// ////import collectionRoutes from "./routes/collectionRoutes.ts";
 
-dotenv.config();
+// dotenv.config();
 
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-console.log("Server je pokrenut");
-// Registrujemo rute
-app.use("/api/recipes", recipeRoutes);
-app.use('/api/milvus', milvusRoutes);
-//app.use('/app/collections', collectionRoutes);
+// console.log("Server je pokrenut");
+// // Registrujemo rute
+// app.use("/api/recipes", recipeRoutes);
+// app.use('/api/milvus', milvusRoutes);
+// //app.use('/app/collections', collectionRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
 
-// Health check
-app.get('/health', (_req, res) => res.send('Server is healthy!'));
+// // Health check
+// app.get('/health', (_req, res) => res.send('Server is healthy!'));
 
-export default app;
+// export default app;
